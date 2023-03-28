bca4431f1bb9d567f4e7c50a39b140cc8a6482a2
Ankesh <iankesh@hotmail.com>
2023-03-29 02:41:33 +0530

    committing automation.sh file

---
+++ automation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt update -y
+
+timestamp=$(date '+%d%m%Y-%H%M%S')
+myname="ankesh"
+s3_bucket="upgrad-ankesh"
+
+apacheInstallStatus=$(dpkg -s apache2 | grep Status | awk '{print $NF}')
+if [ "$apacheInstallStatus" != "installed" ]
+then
+        echo "Apache2 not installed, Instaling it."
+        sudo apt install apache2 -y
+else
+        echo "Apache2 is already installed."
+fi
+
+apacheiServiceStatus=$(service --status-all | grep apache2 | awk '{print $2}')
+if [ "$apacheiServiceStatus" != "+" ]
+then
+        echo "Apache2 is not up, Starting and Enabling it."
+        sudo systemctl restart apache2
+        sudo systemctl enable apache2
+else
+        echo "Apache2 is already up."
+fi
+
+cd /var/log/apache2/ && tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar *.log
+
+aws s3 \
+cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
+s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
